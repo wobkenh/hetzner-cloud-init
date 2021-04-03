4c805dbfb8fdda9a628e05dcc279d62b4351b2d9
Henning Wobken <henning.wobken@simplex24.de>
2021-04-03 15:34:23 +0200

    customization

--- setup.sh
+++ setup.sh
@@ -28,21 +28,14 @@ done
 FLOATING_IPS=${FLOATING_IPS:-""}
 
 
-sed -i 's/[#]*PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
-sed -i 's/[#]*PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-
-systemctl restart sshd
-
 wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x jq-linux64
 mv jq-linux64 /usr/local/bin/jq
 
 
-curl -o /usr/local/bin/update-config.sh https://raw.githubusercontent.com/vitobotta/hetzner-cloud-init/master/update-config.sh
+curl -o /usr/local/bin/update-config.sh https://raw.githubusercontent.com/wobkenh/hetzner-cloud-init/master/update-config.sh
 
 chmod +x /usr/local/bin/update-config.sh
-
-ufw allow proto tcp from any to any port 22,80,443
 
 IFS=', ' read -r -a WHITELIST <<< "$WHITELIST_S"
 
@@ -66,3 +59,4 @@ EOF
 
 /usr/local/bin/update-config.sh --hcloud-token ${TOKEN} --whitelisted-ips ${WHITELIST_S} ${FLOATING_IPS}
 
+echo "Miau ist wuff"
