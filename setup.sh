ff78228e695fd5d7e9b86433533b9a8a87b3b403
Henning Wobken <henning.wobken@simplex24.de>
2021-04-03 15:49:21 +0200

    init

--- setup.sh
+++ setup.sh
@@ -28,6 +28,21 @@ done
 FLOATING_IPS=${FLOATING_IPS:-""}
 
 
+export DEBIAN_FRONTEND=noninteractive
+rm -vrf /var/lib/apt/lists/*
+apt-get update -y -q
+apt-get upgrade -y -q
+apt-get install ufw -y -q
+apt-get install fail2ban -y -q
+printf "[sshd]\nenabled = true\nbanaction = iptables-multiport" > /etc/fail2ban/jail.local
+systemctl enable fail2ban
+sed -i -e '/^PasswordAuthentication/s/^.$/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i -e '/^#MaxAuthTries/s/^.$/MaxAuthTries 2/' /etc/ssh/sshd_config
+systemctl restart sshd
+systemctl restart fail2ban
+
+
+
 wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x jq-linux64
 mv jq-linux64 /usr/local/bin/jq
